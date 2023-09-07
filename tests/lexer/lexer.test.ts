e4c361deca8317bbe5acca036d1feb0c6471d093
Jing Xu(RainX) <i@rainx.cc>
2023-09-07 17:33:32 +0800

    test: update test source for lexer test by using the code from Laravel

--- tests/lexer/lexer.test.ts
+++ tests/lexer/lexer.test.ts
@@ -1,18 +1,14 @@
 import { Lexer } from '../../src';
 
+// Comes from Laravel https://github.com/laravel/framework/blob/10.x/src/Illuminate/Collections/Collection.php
 const commonSource = `
 /**
- * @property float|null $cash_balance
- * @property-read string|null         $default_name
- * @property-read int|null            $refresh_code
- * @property-read \\Carbon\\Carbon|null $refreshed_at
- * @property-read string|null         $refreshed_at_friendly
- * @property-read int                 $signed_balance
- * @property-read int|null            $tax_advantage_id
- * @property-read float               $total_cost_basis total cost to buy, both stocks and funds
- * @property-read float               $total_value      total market value + signed cash balance
+ * @template TKey of array-key
  *
- * @method static \\Illuminate\\Database\\Eloquent\\Builder included()
+ * @template-covariant TValue
+ *
+ * @implements \\ArrayAccess<TKey, TValue>
+ * @implements \\Illuminate\\Support\\Enumerable<TKey, TValue>
  */
 `.trim();
 
